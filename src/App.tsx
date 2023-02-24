90bb43fcc333587283aacacb1fcf9a4eebe2d496
Gabriel Barreto <gabriel.devloper@gmail.com>
2023-02-24 16:47:38 -0300

    feat: componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -8,7 +9,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      Hello World
+      <Transactions></Transactions>
     </ThemeProvider>
   )
 }
---
+++ src/pages/Transactions/Transactions.tsx
@@ -0,0 +1,7 @@
+import { Header } from "../../components/Header/Header";
+
+export function Transactions() {
+    return <div>
+        <Header></Header>
+    </div>
+}
