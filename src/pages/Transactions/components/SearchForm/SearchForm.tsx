a2904727ceff795f2492247ea5e33a3b02adc99b
Gabriel Barreto <gabriel.devloper@gmail.com>
2023-03-01 20:53:57 -0300

    feat: react hook form configurado na aplicação.

--- src/pages/Transactions/components/SearchForm/SearchForm.tsx
+++ src/pages/Transactions/components/SearchForm/SearchForm.tsx
@@ -1,12 +1,39 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
 
-export function SearchForm() {
-    return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+import * as z from "zod";
 
-            <button type="submit">
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+    query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
+
+export function SearchForm() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    });
+
+    async function handleSearchTransaction(data: SearchFormInputs) {
+        await new Promise(resolver => setTimeout(resolver, 2000));
+
+        console.log(data)
+    }
+
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                {...register('query')}
+            />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20} />
                 Buscar
             </button>
--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -9,7 +9,7 @@ export const SearchFormContainer = styled.form`
         border-radius: 6px;
         border: 0;
         background: ${props => props.theme["gray-900"]};
-        color: ${props => props.theme["gray-900"]};
+        color: ${props => props.theme.white};
         padding: 1rem;
 
         &::placeholder {
@@ -22,7 +22,7 @@ export const SearchFormContainer = styled.form`
         display: flex;
         align-items: center;
         gap: 0.75rem;
-
+        cursor: pointer;
         border: 0;
         padding: 1rem;
         background-color: transparent;
@@ -31,7 +31,12 @@ export const SearchFormContainer = styled.form`
         font-weight: bold;
         border-radius: 6px;
 
-        &:hover {
+        &:disabled {
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
+
+        &:not(:disabled):hover {
             background-color: ${props => props.theme["green-500"]};
             border-color: ${props => props.theme["green-500"]};
             color: ${props => props.theme.white};
