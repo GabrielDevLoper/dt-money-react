5ae91239b4ff24a9d7e9f4282e814aa85203b070
Gabriel Barreto <gabriel.devloper@gmail.com>
2023-03-01 15:53:51 -0300

    feat: finalizado a estilização do sistema.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal/NewTransactionModal";
 
 export function Header() {
     return (
@@ -8,7 +10,14 @@ export function Header() {
             <HeaderContent>
                 <img src={logoImg} alt="" />
 
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+
+                    <NewTransactionModal />
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
