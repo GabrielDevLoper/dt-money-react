8aa1d6feaec811c86d48222ecf73c7caa27bd6e6
Gabriel Barreto <gabriel.devloper@gmail.com>
2023-03-02 15:58:36 -0300

    feat: buscando transações.

--- src/pages/Transactions/components/SearchForm/SearchForm.tsx
+++ src/pages/Transactions/components/SearchForm/SearchForm.tsx
@@ -5,6 +5,8 @@ import { SearchFormContainer } from "./styles";
 import * as z from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -14,12 +16,14 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext);
+
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
         resolver: zodResolver(searchFormSchema)
     });
 
     async function handleSearchTransaction(data: SearchFormInputs) {
-        await new Promise(resolver => setTimeout(resolver, 2000));
+        await fetchTransactions(data.query);
 
         console.log(data)
     }
